5055e25ae503380063c5179b285e32fb6dec23d6
Krups <krupal.joshi@gmail.com>
2025-08-10 14:44:52 +0100

    sprite sheet working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { app } from '../libs/renderer/index.js';
-import { loadAssets, getTexture } from '../libs/loader/index.js';
+import { loadTextures } from '../libs/loader/index.js';
 
 
 /**
@@ -10,31 +10,60 @@ export async function init() {
     const stage = app.stage;
 
     // Example: add a test graphic to stage
-    const graphic = new PIXI.Graphics();
-    graphic.beginFill(0xff0000);
-    graphic.drawRect(0, 0, 100, 100);
-    graphic.endFill();
-    stage.addChild(graphic);
+    // const graphic = new PIXI.Graphics();
+    // graphic.beginFill(0xff0000);
+    // graphic.drawRect(0, 0, 100, 100);
+    // graphic.endFill();
+    // stage.addChild(graphic);
 
-    // const img = './assets/face.png'; // Ensure the path is correct
+    const frames = [
+        { name: 'bigFrame', x: 0, y: 0, width: 440, height: 590 },
+        { name: 'balanceBox', x: 444, y: 0, width: 330, height: 106 },
+        { name: 'winningsBox', x: 458, y: 114, width: 330, height: 106 },
+        { name: 'savedWinningsBox', x: 448, y: 225, width: 348, height: 106 },
+        { name: 'spadeCard', x: 440, y: 334, width: 190, height: 264 },
+        { name: 'heartCard', x: 629, y: 334, width: 190, height: 264 },
+        { name: 'diamondCard', x: 818, y: 334, width: 190, height: 264 },
+        { name: 'clubCard', x: 818, y: 599, width: 190, height: 264 },
+        { name: 'backCard', x: 629, y: 599, width: 190, height: 264 },
+        { name: 'poop', x: 629, y: 599, width: 190, height: 264 },
 
-    // const faceAsset = PIXI.Assets.load(img);
+    ];
 
-    // faceAsset.then((myTexture) => {
-    // const sprite = new PIXI.Sprite(myTexture); // Directly use the resolved texture
-    // sprite.x = 100;
-    // sprite.y = 100;
-    // sprite.scale.set(0.5);
+    const textures = await loadTextures('../../assets/PairsSheet.png', frames);
 
-    // app.stage.addChild(sprite);
-    // });
-    await loadAssets();
+    const scaleContainer = new PIXI.Container();
+    stage.addChild(scaleContainer);
 
-    const card = new PIXI.Sprite(getTexture('pairsSheet'));
-    card.x = 100;
-    card.y = 100;
+    const poopy = new PIXI.Sprite(textures.poop);
+    poopy.x = 0;
+    poopy.y = 0;
+    poopy.scale = 1;
+    scaleContainer.addChild(poopy);
 
-    app.stage.addChild(card);
 
+
+    makeCards(scaleContainer, textures);
 }
 
+function makeCards(scaleContainer, textures) {
+    const spade = new PIXI.Sprite(textures.spadeCard);
+    spade.x = 0;
+    spade.y = 270;
+    scaleContainer.addChild(spade);
+
+    const heart = new PIXI.Sprite(textures.heartCard);
+    heart.x = 200;
+    heart.y = 270;
+    scaleContainer.addChild(heart);
+
+    const diamond = new PIXI.Sprite(textures.diamondCard);
+    diamond.x = 400;
+    diamond.y = 270;
+    scaleContainer.addChild(diamond);
+
+    const club = new PIXI.Sprite(textures.clubCard);
+    club.x = 600;
+    club.y = 270;
+    scaleContainer.addChild(club);
+}
